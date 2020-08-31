de93213f99a54b0adb1fc055707cc6358b219220
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2020-08-31 12:27:50 +0200

    Hello Niklas

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            int age = 18;
+            string name = "Max";
+            bool alive = true;
             Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Alive? " + alive);
         }
     }
 }
