e95511dae8f676bd8462f7f8bc1c17dd87f291d3
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2020-09-03 13:07:25 +0200

    Update

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -2,5 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const clientDir = __dirname + "\\client\\"
+
+app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
+app.get('/style', (req, res) => res.sendFile(clientDir + "style.css"))
+app.get('/teknik', (req, res) => res.send('EK sux'))
+app.get('/yeezus', (req, res) => res.sendFile(clientDir + "haha.jpg"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
